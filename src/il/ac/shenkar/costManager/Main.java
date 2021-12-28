d61e40ef08c77f2841e6ed71d4956b5f31c195df
Ofir <ofir.duchovne@gmail.com>
2021-12-28 17:14:45 +0200

    Done with prepareStatement and try with resources

--- src/il/ac/shenkar/costManager/Main.java
+++ src/il/ac/shenkar/costManager/Main.java
@@ -1,6 +1,7 @@
 package il.ac.shenkar.costManager;
 
-import java.util.Properties;
+import java.util.Date;
+import java.util.LinkedList;
 
 public class Main {
 
@@ -8,16 +9,19 @@ public class Main {
         try {
             SimpleDBModel m = new SimpleDBModel();
             User user = m.login("arthur.dent@email.com","12345");
-            Properties properties = new Properties();
+//            Properties properties = new Properties();
 
 //            System.out.println(user.getName());
 //            m.addCategory(new Category(user,"food",0));
 //            LinkedList<Category> categories = (LinkedList<Category>) m.getCategories(user);
 //            Category category = categories.pop();
 //            System.out.println(category);
-//            m.addItem(new Item(0,user.getUserId(),"test item","bla bla bla bla", 1, 27.3, new Date(),category));
-//            LinkedList<Item> items = (LinkedList<Item>) m.getItems(user);
-//            m.deleteItems(items.pop());
+            m.addItem(new Item(0,user.getUserId(),"test item","okokok", 1, 27.3, new Date(),new Category(user,"food",2)));
+            LinkedList<Item> items = (LinkedList<Item>) m.getItems(user);
+            for (Item item : items) {
+                System.out.println(item);
+            }
+            m.deleteItems(items.pop());
 //            System.out.println(items);
         } catch (CostManagerException e) {
             e.printStackTrace();
