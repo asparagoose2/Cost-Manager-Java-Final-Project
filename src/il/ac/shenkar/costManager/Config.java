328349b4c0ead2a3281bb8a152ef9b27dc35b678
Ofir Duchovne <72769605+asparagoose2@users.noreply.github.com>
2022-02-03 15:54:30 +0200

    Update Config.java

--- src/il/ac/shenkar/costManager/Config.java
+++ src/il/ac/shenkar/costManager/Config.java
@@ -4,6 +4,7 @@ public class Config {
     static public final String DB_NAME = "CostManager";
     static public final String DB_USER = "<username>";
     static public final String DB_PASSWORD = "<password>";
-    static public final String DB_URL = "jdbc:mysql://<ip and port>/" + DB_NAME;
+    static public final String DB_ONLY_URL = "jdbc:mysql://<ip and port>/";
+    static public final String DB_URL = DB_ONLY_URL + DB_NAME;
     static public final String DB_DRIVER = "com.mysql.jdbc.Driver";
 }
