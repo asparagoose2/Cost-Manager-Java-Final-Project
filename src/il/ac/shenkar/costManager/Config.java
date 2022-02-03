d8115a5988246336472dbdb93a72874b997a0742
Ofir <ofir.duchovne@gmail.com>
2022-02-03 15:26:33 +0200

    config

---
+++ src/il/ac/shenkar/costManager/Config.java
@@ -0,0 +1,9 @@
+package il.ac.shenkar.costManager;
+
+public class Config {
+    static public final String DB_NAME = "CostManager";
+    static public final String DB_USER = "<username>";
+    static public final String DB_PASSWORD = "<password>";
+    static public final String DB_URL = "jdbc:mysql://<ip and port>/" + DB_NAME;
+    static public final String DB_DRIVER = "com.mysql.jdbc.Driver";
+}
